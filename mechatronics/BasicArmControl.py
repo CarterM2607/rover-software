26c05edefbf194f29473b26e504647cb6898deb1
kameshwarasekar <kameshwarasekar@gmail.com>
2021-08-25 21:16:53 -0600

    Code cleanup

--- mechatronics/BasicArmControl.py
+++ mechatronics/BasicArmControl.py
@@ -6,8 +6,8 @@ from Phidget22.Devices.Stepper import *
 
 from pynput import keyboard
 from pynput.keyboard import Key, Listener
-#import os
-#import shutil
+# import os
+# import shutil
 import traceback
 import time
 
@@ -69,24 +69,24 @@ def on_release(key):
     global base_motor, shoulder_motor1, shoulder_motor2, elbow_motor, wrist_motor, claw_motor
     try:
         # Base motor off
-        if (key.char == 'q' or key.char == 'w'):
+        if key.char == 'q' or key.char == 'w':
             base_motor.setVelocityLimit(0)
 
         # Shoulder motors off
-        if (key.char == 'e' or key.char == 'r'):
+        if key.char == 'e' or key.char == 'r':
             shoulder_motor1.setVelocityLimit(0)
             shoulder_motor2.setVelocityLimit(0)
 
         # Elbow motor off
-        if (key.char == 'a' or key.char == 's'):
+        if key.char == 'a' or key.char == 's':
             elbow_motor.setVelocityLimit(0)
 
         # Wrist motor off
-        if (key.char == 'd' or key.char == 'f'):
+        if key.char == 'd' or key.char == 'f':
             wrist_motor.setVelocityLimit(0)
 
         # Claw motor off
-        if (key.char == 'g' or key.char == 'h'):
+        if key.char == 'g' or key.char == 'h':
             claw_motor.setVelocityLimit(0)
 
         # End listener on ESC
@@ -102,18 +102,22 @@ def on_release(key):
 
     except AttributeError:
         print("Special key {0} released".format(key))
-        
+
+
 def onAttach(motor_number):
     print("Motor {0} attached!".format(motor_number))
 
+
 def onDetach(self):
     print("Detach!")
-    #print("Motor {0} detached!".format(motor_number))
+    # print("Motor {0} detached!".format(motor_number))
 
-def onError(self,code, description):
+
+def onError(self, code, description):
     print("Code: " + ErrorEventCode.getName(code))
     print("Description: " + str(description))
     print("----------")
+
 
 def initialize_motors(motors, motors_info):
     for i in range(len(motors)):
@@ -126,43 +130,42 @@ def initialize_motors(motors, motors_info):
         motors[i].setControlMode(StepperControlMode.CONTROL_MODE_RUN)
         motors[i].setCurrentLimit(motors_info[i][0])
         motors[i].setHoldingCurrentLimit(motors_info[i][1])
-        motors[i].setRescaleFactor(1.8/ 16/motors_info[i][2])
+        motors[i].setRescaleFactor(1.8 / 16 / motors_info[i][2])
         motors[i].setAcceleration(50)
         motors[i].setVelocityLimit(0)
         motors[i].setEngaged(True)
         motors[i].setDataInterval(motors[i].getMinDataInterval())
 
 
-
-# -----------------------------------------------------  MAIN  ----------------------------------------------------------------------
+# -----------------------------------------------------  MAIN  --------------------------------------------------------
 # Declare and initialize motors and motor info (current limit, holding current, gear ratio)
-base_motor = Stepper()           # Rotates base
+base_motor = Stepper()  # Rotates base
 base_motor_info = [2.8, 1, 77]
-shoulder_motor1 = Stepper()      # Reference motor to rotate shoulder joint
+shoulder_motor1 = Stepper()  # Reference motor to rotate shoulder joint
 shoulder_motor1_info = [2.8, 2.8, 15]
-shoulder_motor2 = Stepper()      # Support motor to rotate shoulder joint (inverse of shoulder_motor1)
+shoulder_motor2 = Stepper()  # Support motor to rotate shoulder joint (inverse of shoulder_motor1)
 shoulder_motor2_info = [2.8, 0, 15]
-elbow_motor = Stepper()          # Rotates elbow
+elbow_motor = Stepper()  # Rotates elbow
 elbow_motor_info = [2.8, 1, 15]
-wrist_motor = Stepper()          # Rotates wrist
+wrist_motor = Stepper()  # Rotates wrist
 wrist_motor_info = [0.67, 0.67, 100]
-claw_motor = Stepper()           # Pinches claw
+claw_motor = Stepper()  # Pinches claw
 claw_motor_info = [0.67, 0, 100]
-             
-all_motor_names = [base_motor, shoulder_motor1, shoulder_motor2, elbow_motor, wrist_motor, claw_motor]
-all_motor_info = [base_motor_info, shoulder_motor1_info, shoulder_motor2_info, elbow_motor_info, wrist_motor_info, claw_motor_info]
 
+all_motor_names = [base_motor, shoulder_motor1, shoulder_motor2, elbow_motor, wrist_motor, claw_motor]
+all_motor_info = [base_motor_info, shoulder_motor1_info, shoulder_motor2_info, elbow_motor_info, wrist_motor_info,
+                  claw_motor_info]
 
 try:
     Log.enable(LogLevel.PHIDGET_LOG_INFO, "PhidgetArmLog.log")
     initialize_motors(all_motor_names, all_motor_info)
 
     # Collect events until released
-    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:        
-        listener.join()              
-    #try:
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+        # try:
     #    input("Press Enter to Stop\n")
-    #except (Exception, KeyboardInterrupt):
+    # except (Exception, KeyboardInterrupt):
     #    pass
 
     base_motor.close()
@@ -173,10 +176,9 @@ try:
     claw_motor.close()
 
 except PhidgetException as ex:
-    #We will catch Phidget Exceptions here, and print the error informaiton.
+    # We will catch Phidget Exceptions here, and print the error information.
     traceback.print_exc()
     print("")
     print("PhidgetException " + str(ex.code) + " (" + ex.description + "): " + ex.details)
-
 
 print("DONE")
